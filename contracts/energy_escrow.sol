1a66e4f38cb731df875e1b075e7f7c07d0f43aa2
Duarte Aragao <daragao@gmail.com>
2018-09-18 01:44:59 +0100

    WIP: withdraw from escrow with proof (signd by producer and distributor)

--- contracts/energy_escrow.sol
+++ contracts/energy_escrow.sol
@@ -8,10 +8,11 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Holder.sol';
 // get rewarded for energy production
 // it exchanges EnergyTokens for ETH for simplicity, but we would probably want to have our own token
 contract EnergyEscrow is ERC721Holder {
-  event PaymentCreation(uint256 indexed tokenId, address indexed producer, address indexed comsumer, uint value);
+  event PaymentCreation(uint256 tokenId, address producer, address distributor, address comsumer, uint value);
   enum PaymentStatus { Pending, Completed, Refunded }
   struct Payment {
     address producer;
+    address distributor;
     address consumer;
     uint value;
     PaymentStatus status;
@@ -26,12 +27,23 @@ contract EnergyEscrow is ERC721Holder {
   }
 
   // need to specify consumer since this will be executed by the order book contract
-  function createPayment(uint256 _tokenId, address _consumer) external payable {
+  function createPayment(uint256 _tokenId, address _consumer, address _distributor) external payable {
     address producer = energyToken.ownerOf(_tokenId);
     uint value = msg.value;
     if(msg.value == 0) { revert("Need to send some funds"); }
     energyToken.safeTransferFrom(producer, address(this), _tokenId); // THIS IS FAILING DO TO NOT BEING APPROVED
-    payments[_tokenId] = Payment(producer, _consumer, value, PaymentStatus.Pending);
-    emit PaymentCreation(_tokenId, producer, _consumer, value);
+    payments[_tokenId] = Payment(producer, _distributor, _consumer, value, PaymentStatus.Pending);
+    emit PaymentCreation(_tokenId, producer, _distributor, _consumer, value);
+  }
+
+  function testRecovery(bytes32 h, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
+    /* prefix might be needed for geth only
+     * https://github.com/ethereum/go-ethereum/issues/3731
+     */
+    // bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+    // h = sha3(prefix, h);
+    address addr = ecrecover(h, v, r, s);
+
+    return addr;
   }
 }
--- test/EnergyToken.test.js
+++ test/EnergyToken.test.js
@@ -1,16 +1,25 @@
+const Web3Utils = require('web3-utils'); // need a more recent version of web3 for the signaatures
+const Web3Eth = require('web3-eth'); // need a more recent version of web3 for the signaatures
+
 const EnergyToken = artifacts.require('EnergyToken');
 const EnergyEscrow = artifacts.require('EnergyEscrow');
 
 contract('EnergyToken', accounts => {
-  let token;
-  const creator = accounts[0];
+
+  const creatorPrivateKey = '0x1546e1a27353b3ab3dce3d94faf150cd1f5b86c25c04ae8191d13ec7a844e479'
   const producerPrivateKey = '0xc35fd6f2aace6e3e62f02844c846fe5a4837b8230d53ca3d417af64cb7c20457'
   const consumerPrivateKey = '0xc9099835a49ba3fff15f14ce62bbdbf464768a57b7a212291c9251d8306108e4'
-  const producer = web3.eth.accounts.privateKeyToAccount(producerPrivateKey)
-  const consumer = web3.eth.accounts.privateKeyToAccount(consumerPrivateKey)
+  const distributorPrivateKey = '0x169187e44a8231cfd22b25e393b542fecb93b03dead9806540aab5dcb6959b73'
 
+  const web3Eth = new Web3Eth(web3.currentProvider);
+  const creator = web3Eth.accounts.privateKeyToAccount(creatorPrivateKey);
+  const producer = web3Eth.accounts.privateKeyToAccount(producerPrivateKey)
+  const consumer = web3Eth.accounts.privateKeyToAccount(consumerPrivateKey)
+  const distributor = web3Eth.accounts.privateKeyToAccount(distributorPrivateKey)
+
+  let token;
   beforeEach(async function () {
-    token = await EnergyToken.new('EnergyToken','BLK',{ from: creator });
+    token = await EnergyToken.new('EnergyToken','BLK',{ from: creator.address });
   });
 
   it('has a name', async function () {
@@ -26,21 +35,18 @@ contract('EnergyToken', accounts => {
     await token.mint(consumer.address,2);
     totalSupply = await token.totalSupply();
     assert.equal(totalSupply, 3);
-    let balanceOf2 = await token.balanceOf(accounts[2])
+    let balanceOf2 = await token.balanceOf(consumer.address)
     assert.equal(balanceOf2.toNumber(), 1);
 
     await token.burn(0);
      totalSupply = await token.totalSupply();
     assert.equal(totalSupply, 2);
-    const balanceOf1 = await token.balanceOf(accounts[1])
+    const balanceOf1 = await token.balanceOf(producer.address)
     assert.equal(balanceOf1.toNumber(), 1);
   })
 
   it('energy escrow deposit', async () => {
-    const escrow = await EnergyEscrow.new(token.address,{ from: creator });
-
-    const producer = accounts[1];
-    const consumer = consumer.address;
+    const escrow = await EnergyEscrow.new(token.address,{ from: creator.address });
 
     await token.mint(producer.address,0);
     await token.mint(producer.address,1);
@@ -49,11 +55,24 @@ contract('EnergyToken', accounts => {
 
     const depositValue = 10;
     // consumer (or market book order pays into escrow)
-    const tx = await escrow.createPayment(0,consumer.address,{ from: consumer.address, value: depositValue })
-    let escrowETHBalance = await web3.eth.getBalance(escrow.address);
+    const tx = await escrow.createPayment(0, distributor.address,consumer.address,{ from: consumer.address, value: depositValue })
+    let escrowETHBalance = await web3Eth.getBalance(escrow.address);
     assert.equal(escrowETHBalance,depositValue)
   })
 
-  it('energy escrow withdraw', async () => {
+  it.only('test signature', async () => {
+    const tokenId = 0;
+    const concatHash = Web3Utils.soliditySha3(tokenId)
+    const msg = new Buffer(concatHash.slice(2),16)
+
+    const prefix = new Buffer("\x19Ethereum Signed Message:\n");
+    const signature = producer.sign(msg);
+
+    const prefixedMsg = Web3Utils.sha3(Buffer.concat([prefix, new Buffer(String(msg.length)), msg]));
+
+    const escrow = await EnergyEscrow.new(token.address,{ from: creator.address });
+    const recoveredAddr = await escrow.testRecovery.call(prefixedMsg,signature.v,signature.r,signature.s)
+
+    assert.equal(producer.address.toLowerCase(),recoveredAddr.toLowerCase())
   })
 })
